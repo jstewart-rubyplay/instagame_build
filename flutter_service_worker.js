e865b675233b6d7b5c802c45a968d96f899cc05c
james.stewart4 <james.stewart@rubyplay.com>
2025-04-22 10:26:31 +1000

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2f7528cd7b54b944da657621d7081aff",
+const RESOURCES = {"flutter_bootstrap.js": "d27bd99cfc98a03e229dd0e883e074f8",
 "version.json": "bfecad57a3f6090754cffa82f7d77950",
 "index.html": "28e2c6b7b112aad0c66e42a0d5e13f45",
 "/": "28e2c6b7b112aad0c66e42a0d5e13f45",
