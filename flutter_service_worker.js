bb23d01ed466adba8e7fc79396747f0250e0bbba
james.stewart4 <james.stewart@rubyplay.com>
2025-10-27 15:43:27 +1100

    updated - drop ball volatility selection

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8b70fa878fd948603b6478ed1c71f72e",
+const RESOURCES = {"flutter_bootstrap.js": "0fb9e0bd2186fa75d335eb8fa5552c87",
 "version.json": "bfecad57a3f6090754cffa82f7d77950",
 "index.html": "770414b67cd3dfda1a5609d584750813",
 "/": "770414b67cd3dfda1a5609d584750813",
-"main.dart.js": "edc3dc5f8052af4241f6132d6dbe885d",
+"main.dart.js": "eab709d3f83938ca458ff010d0bc0cd1",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
