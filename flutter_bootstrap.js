435198d5e344e36667dd952c6012656a0fa7b1ab
james.stewart4 <james.stewart@rubyplay.com>
2025-10-28 18:51:44 +1100

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3499668679"
+    serviceWorkerVersion: "3560310988"
   }
 });
