7de01b07655572deb622dc7ed7562cd396a31a4d
james.stewart4 <james.stewart@rubyplay.com>
2026-02-26 18:15:24 +1100

    update - basic reel spin

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "df45f081ec48b65cb8840dfb70482a30",
+const RESOURCES = {"flutter_bootstrap.js": "6db2e277bdeb6ce97b553feb1bb56d5c",
 "version.json": "bfecad57a3f6090754cffa82f7d77950",
 "index.html": "770414b67cd3dfda1a5609d584750813",
 "/": "770414b67cd3dfda1a5609d584750813",
-"main.dart.js": "80dee8d2ec1da2d7adc6fbd16e87d1c8",
+"main.dart.js": "a0e6f32f2d490cecbd472b878ca20123",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
